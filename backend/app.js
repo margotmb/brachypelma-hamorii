2817afdb574a0e81b699642348a75a9042eaebf1
Margot M. Baisch <73503491+margotmb@users.noreply.github.com>
2024-04-11 11:27:22 +0000

    Updated new routes

--- backend/app.js
+++ backend/app.js
@@ -1,52 +1,56 @@
 const express = require("express");
-const cors = require('cors');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
+const cors = require("cors");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 const app = express();
+const router = express.Router();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors());
 
 //ConnectDB
 const mongoose = require("mongoose");
 mongoose.connect(
-    process.env.MONGODB_URI = "", 
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        dbName: 'catdoorlogs',
-    },
-    console.log("CONNECTED")
+  (process.env.MONGODB_URI = ""),
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "planteventlog",
+  },
+  console.log("CONNECTED"),
 );
 
 // Sessions Middleware Configuration
 const store = new MongoDBStore({
-    uri: '',
-    collection: 'sessions'
-  });
+  uri: "",
+  collection: "sessions",
+});
 
-app.use(session({
-    secret: '',
+app.use(
+  session({
+    secret: "",
     resave: false,
     saveUninitialized: true,
     cookie: { secure: true },
-    store: store
-}));
-
+    store: store,
+  }),
+);
 
 // routes
-const catdoorevents = require('./routes/catdoorevents');
-const sessions = require('./routes/sessions');
+const plantevents = require("./routes/plantevents");
+const sessions = require("./routes/sessions");
 
-app.use('/catdoorevents', catdoorevents);
-app.use('/sessions', sessions);
+app.use("/plantevents", plantevents);
+app.use("/sessions", sessions);
 
-
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 //PORT SET
 var port = 5000;
 module.exports = app;
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-  })
+  console.log(`Listening on port ${port}`);
+});
