c5ff4e0b843a0acddf8834f781813d6dfc636e76
Margot M. Baisch <73503491+margotmb@users.noreply.github.com>
2024-04-22 12:13:14 +0000

    refactor

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
 const app = express();
-const router = express.Router();
+const dotenv = require("dotenv");
+dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,7 +11,7 @@ app.use(cors());
 //ConnectDB
 const mongoose = require("mongoose");
 mongoose.connect(
-  (process.env.MONGODB_URI = ""),
+  process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -21,28 +20,10 @@ mongoose.connect(
   console.log("CONNECTED"),
 );
 
-// Sessions Middleware Configuration
-const store = new MongoDBStore({
-  uri: "",
-  collection: "sessions",
-});
-
-app.use(
-  session({
-    secret: "",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true },
-    store: store,
-  }),
-);
-
 // routes
 const plantevents = require("./routes/plantevents");
-const sessions = require("./routes/sessions");
 
 app.use("/plantevents", plantevents);
-app.use("/sessions", sessions);
 
 app.get("/", (req, res) => {
   res.send("hello world");
