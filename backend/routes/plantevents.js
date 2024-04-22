f695b350503ca6808e52a0c807e3fb390e740f31
Margot M. Baisch <73503491+margotmb@users.noreply.github.com>
2024-04-22 14:36:50 +0000

    refactor route

--- backend/routes/plantevents.js
+++ backend/routes/plantevents.js
@@ -1,35 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const PlantEvent = require("../models/PlantEvent");
-const Device = require("../models/Device");
 
 // @route GET
-// @description Get all events
+// @description Get all events from current device id
 router.get("/", (req, res) => {
-  Device.findOne({ deviceId: req.params.deviceId }).then((identity) => {
-    //add permission by deviceid
-    console.log(identity);
-    PlantEvent.find()
-      .then((PlantEvents) => res.json(PlantEvents))
-      .catch((err) =>
-        res.status(404).json({ noplanteventsfound: "No Events found" }),
-      );
-  });
+  PlantEvent.find({ deviceId: req.params.deviceId })
+    .then((PlantEvents) => res.json(PlantEvents))
+    .catch((err) =>
+      res.status(404).json({ noplanteventsfound: "No Events found" }),
+    );
 });
 
 // @route POST
 // @description add/save event to DB, needs deviceId
-// @access Public
+// @access Public -> Create Collection for Device Permission
+// Check if device is on the database, deviceId stays on cookie for querys and checks
 router.post("/", (req, res) => {
-  Device.findOne({ deviceId: req.params.deviceId }).then((identity) => {
-    //add permission by deviceid
-    console.log(identity);
-    PlantEvent.create(req.body)
-      .then(res.json({ msg: "Event added successfully" }))
-      .catch((err) =>
-        res.status(400).json({ error: "Unable to add this event" }),
-      );
-  });
+  PlantEvent.create(req.body)
+    .then(res.json({ msg: "Event added successfully" }))
+    .catch((err) =>
+      res.status(400).json({ error: "Unable to add this event" }),
+    );
 });
 
 module.exports = router;
