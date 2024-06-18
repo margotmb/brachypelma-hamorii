47b989eda186b1e533dde26aec78d4e1654f2a1f
Margot <margot.mb@outlook.com>
2024-06-18 12:14:52 -0300

    Testing Tables with Fetch

--- frontend/components/DataTable.tsx
+++ frontend/components/DataTable.tsx
@@ -1,25 +1,46 @@
 
-import React, { useState } from 'react'; 
+import React, { useState, useEffect } from 'react'; 
 import { StyleSheet, View } from 'react-native';
 import { Table, Row, Rows } from 'react-native-table-component';
 
-const data = [
-    { id: 1, name: 'John Doe', age: 30 },
-    { id: 2, name: 'Jane Smith', age: 25 },
-    { id: 3, name: 'Bob Johnson', age: 35 }
-  ];
-
-const tableData = {
+/*
+  SampleData:
+  tableData = {
     tableHead: ['Temp ºC', 'Umidade %', 'Luz', 'DataHora'],
     tableData: [
       ['25', '80', 'true', '15:30 15/06/2024'],
       ['27', '70', 'false','14:30 15/06/2024'],
       ['28', '60', 'true','13:30 15/06/2024'],
     ],
-}
-  
+*/   
+
+
+let tableData = {tableHead: [''], tableData: [['']]}
+
 const TableExample = () => { 
   const [data, setData] = useState(tableData);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => fetch('https://universalis.app/api/v2/extra/stats/least-recently-updated?world=siren&dcName=aether&entries=10')
+                .then(data => data.json())
+                .then((data) => {
+                  console.log(data['items'][0]);
+                  data = data['items']
+                  let dataArray = []
+                  for(var i = 0; i<=9; i++){
+                    dataArray.push([data[i]['itemID'], data[i]['lastUploadTime'], data[i]['worldID'], data[i]['worldName']])
+                  }
+                  
+                  tableData = {
+                    tableHead: ['itemID', 'lastUploadTime', 'worldID', 'worldName'],
+                    tableData: dataArray,
+                  }
+                  setData(tableData)
+                })
+              
   return (
       <View style={styles.container}>
           <Table borderStyle={{ borderWidth: 2, borderColor: 'orange' }}>
@@ -30,8 +51,7 @@ const TableExample = () => {
   )
 }; 
   
-// npm install react-native-table-component
-// https://www.waldo.com/blog/react-native-table
+
   
 const styles = StyleSheet.create({
   container: { flex: 1, marginBottom: 15, padding: 0, justifyContent: 'center', backgroundColor: '#fff' },
